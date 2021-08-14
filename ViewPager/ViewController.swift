93918f8d4201d87eb4b96defc5219a822a1cbb54
zafarivaev <z.ivaev@mail.ru>
2021-08-14 17:34:55 +0500

    Initial Commit

---
+++ ViewPager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewPager
+//
+//  Created by Zafar on 14/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
